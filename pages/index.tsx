b30ba6c07bac00b3db76c4075ba0e2e072d8d1e8
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-12 23:18:44 +0100

    chore: added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,76 @@ import "tw-elements";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Dublin Hospitality jobs{" "}
-      </h1>
+      <section className="mb-40">
+        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+          <div className="px-6 w-full flex flex-wrap items-center justify-between">
+            <div className="flex items-center">
+              {/*               <button
+                className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContentY"
+                aria-controls="navbarSupportedContentY"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  className="w-5"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+                  ></path>
+                </svg>
+              </button> */}
+              <a className="navbar-brand text-blue-600" href="#!">
+                <div className=" bg-gray-50 text-gray-800 px-6">
+                  <h1 className="text-1xl md:text-2xl xl:text-1xl font-bold tracking-tight">
+                    Dublin <br />
+                    <span className="text-blue-600">Hospitality jobs</span>
+                  </h1>
+                </div>
+              </a>
+            </div>
+            <div
+              className="navbar-collapse collapse grow items-center"
+              id="navbarSupportedContentY"
+            >
+              {/*               <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
+                <li className="nav-item">
+                  <a
+                    className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    Dashboard
+                  </a>
+                </li>
+
+              </ul> */}
+            </div>
+            <div className="flex items-center lg:ml-auto">
+              <button
+                type="button"
+                className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                Post Job
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        <div className="text-center bg-gray-50 text-gray-800 py-24 h-screen px-6"></div>
+      </section>
     </div>
   );
 };
