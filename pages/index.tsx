6fa07e3342d24352dbfd807cd6197cbc4add736d
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-13 16:43:55 +0100

    chore: adding new layout and input/testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,30 +8,14 @@ import Layout from "../components/App/Layout";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <section className="mb-40">
-        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-slate-900 relative flex items-center w-full justify-center">
-          <div className=" lg:ml-auto px-6">
-            <button
-              type="button"
-              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-bold  text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Post Job
-            </button>
-          </div>
-        </nav>
-
-        <div className="flex flex-wrap justify-center bg-slate-900">
-          <img
-            src="https://res.cloudinary.com/dtycp96d8/image/upload/v1657723013/89814f856c3047c1b6705124d50f75f4_7_xxtbci.png"
-            className="max-w-sm w-80 h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl"
-            alt=""
-          />
-        </div>
-
-        <div className="text-center bg-gray-50 text-blue-600 font-bold  text-2xl  py-16 h-screen px-6">
-          Latest hospitality jobs in Dublin...
-        </div>
-      </section>
+      <div className="text-center bg-gray-100 text-blue-600 font-bold  text-2xl  py-16 h-screen px-6">
+        <h1>Latest hospitality jobs in Dublin...</h1>
+        <input
+          type="text "
+          className="px-2 shadow-md rounded py-1 w-80 mt-8"
+          placeholder="search job"
+        />
+      </div>
     </Layout>
   );
 };
