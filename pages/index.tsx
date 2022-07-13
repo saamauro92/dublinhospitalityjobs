fcd872765bf195bee315ee173fdfbccb42203f82
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-13 15:50:49 +0100

    chore: adding logo and proccess of layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import "tw-elements";
 import Layout from "../components/App/Layout";
+
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div>
-        <section className="mb-40">
-          <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
-            <div className="px-6 w-full flex flex-wrap items-center justify-between">
-              <div className="flex items-center">
-                {/*               <button
-                className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContentY"
-                aria-controls="navbarSupportedContentY"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  className="w-5"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-                  ></path>
-                </svg>
-              </button> */}
-                <a className="navbar-brand text-blue-600" href="#!">
-                  <div className=" bg-gray-50 text-gray-800 px-6">
-                    <h1 className="text-1xl md:text-2xl xl:text-1xl font-bold tracking-tight">
-                      Dublin <br />
-                      <span className="text-blue-600">Hospitality jobs</span>
-                    </h1>
-                  </div>
-                </a>
-              </div>
-              <div
-                className="navbar-collapse collapse grow items-center"
-                id="navbarSupportedContentY"
-              >
-                {/*               <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
-                <li className="nav-item">
-                  <a
-                    className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-                    href="#!"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light"
-                  >
-                    Dashboard
-                  </a>
-                </li>
+      <section className="mb-40">
+        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-slate-900 relative flex items-center w-full justify-center">
+          <div className=" lg:ml-auto px-6">
+            <button
+              type="button"
+              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-bold  text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Post Job
+            </button>
+          </div>
+        </nav>
 
-              </ul> */}
-              </div>
-              <div className="flex items-center lg:ml-auto">
-                <button
-                  type="button"
-                  className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  Post Job
-                </button>
-              </div>
-            </div>
-          </nav>
+        <div className="flex flex-wrap justify-center bg-slate-900">
+          <img
+            src="https://res.cloudinary.com/dtycp96d8/image/upload/v1657723013/89814f856c3047c1b6705124d50f75f4_7_xxtbci.png"
+            className="max-w-sm w-80 h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl"
+            alt=""
+          />
+        </div>
 
-          <div className="text-center bg-gray-50 text-gray-800 py-24 h-screen px-6"></div>
-        </section>
-      </div>
+        <div className="text-center bg-gray-50 text-blue-600 font-bold  text-2xl  py-16 h-screen px-6">
+          Latest hospitality jobs in Dublin...
+        </div>
+      </section>
     </Layout>
   );
 };
