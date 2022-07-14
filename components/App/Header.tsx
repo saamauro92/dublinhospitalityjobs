343ad077664fdeab5e3b02ce118e0182c6465c17
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-14 12:30:17 +0100

    chore: added inspirational phrase

--- components/App/Header.tsx
+++ components/App/Header.tsx
@@ -5,14 +5,19 @@ import React, { ReactNode } from "react";
 const Header = (): JSX.Element => {
   return (
     <>
-      <div className="flex flex-wra justify-center bg-slate-900  shadow-md">
+      <div className="text-center bg-slate-900  shadow-md">
         <Link href="/" className=" cursor-pointer">
           <img
             src="https://res.cloudinary.com/dtycp96d8/image/upload/v1657723013/89814f856c3047c1b6705124d50f75f4_7_xxtbci.png"
-            className="max-w-sm w-80 h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl  cursor-pointer"
+            className="max-w-sm w-80 h-auto m-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl  cursor-pointer"
             alt=""
           />
         </Link>
+
+        <h2 className="text-gray-300 font-bold pb-3 ">
+          “Your most unhappy customers are your greatest source of learning.” -
+          Bill Gates
+        </h2>
       </div>
     </>
   );
