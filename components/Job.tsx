8f66ef1fc81a098696d62270693a12cec6382d39
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-14 10:03:38 +0100

    chore: adding icons/improving ui

--- components/Job.tsx
+++ components/Job.tsx
@@ -7,12 +7,24 @@ type Props = {
 const Job = ({ data }: Props): JSX.Element => {
   return (
     <>
-      <div className="mx-4 my-4 mb-14 pb-12 border-b-2">
-        <h2 className="font-semibold text-md my-2 ">{data.title} </h2>
-        <p className=" text-md my-2 ">{data.location}</p>
-        <p className=" text-md my-2 ">{data.description}</p>
-        <p className=" text-md my-2 ">{data.rate}</p>
-        <p className=" text-md my-2 ">{data.start}</p>
+      <div className="mx-4  my-14 pb-12 border-b-2 ">
+        <h2 className="font-semibold text-md my-2 capitalize">{data.title} </h2>
+
+        <div className="flex space-x-2 items-en  items-baseline ">
+          <i className="fas fa-map-marker-alt"></i>
+          <p className=" text-md my-2 capitalize ">{data.location}</p>
+        </div>
+        <div className="flex space-x-2 items-en  items-baseline ">
+          <i className="fas fa-euro-sign"></i>
+
+          <p className=" text-md my-2 capitalize">{data.rate}</p>
+        </div>
+
+        <p className=" text-md my-2  capitalize">{data.description}</p>
+
+        <p className=" text-md my-2 capitalize float-left">
+          Posted: {data.start}
+        </p>
 
         <button className="float-right px-6 py-2.5 bg-blue-600 text-white font-bold  text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
           Read More / Apply
