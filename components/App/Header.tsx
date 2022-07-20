d17a252a343a6c18b0aab352c55490850c0b6aa6
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-20 17:55:25 +0100

    fix: margin bottom from header

--- components/App/Header.tsx
+++ components/App/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({ children, headerBig }: Props): JSX.Element => {
   return (
     <>
       <div
-        className="p-0 text-center relative overflow-hidden bg-no-repeat  bg-center bg-cover  "
+        className="p-0 text-center relative overflow-hidden bg-no-repeat  bg-center bg-cover mb-5 md:mb-5 lg:mb-0 "
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dtycp96d8/image/upload/v1658331164/pexels-luciann-photography-3566191_1_qhokvw.jpg')",
@@ -28,7 +28,7 @@ const Header = ({ children, headerBig }: Props): JSX.Element => {
         >
           <div className="flex justify-center items-center h-full">
             <div className="text-white px-5 lg:px-0 md:px-0 text-left">
-              <h4 className="font-semibold text-base sm:text-xl sm:w-[24rem] md:text-2xl md:w-[30rem] lg:text-2xl w-70 lg:w-[30rem] ">
+              <h4 className="font-semibold text-base w-[18rem] sm:text-xl sm:w-[24rem] md:text-2xl md:w-[30rem] lg:text-2xl w-70 lg:w-[30rem] ">
                 {" "}
                 {phrase}{" "}
               </h4>
