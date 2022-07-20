ff97befc6c77d3a122d25a219897f354be0c85b2
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-20 20:35:18 +0100

    chore: improving styles of input

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -11,7 +11,7 @@ const SearchInput = ({ name, handleSearch, setName }: Props): JSX.Element => {
     <>
       <div className="text-center  text-blue-600 font-bold  text-2xl  pb-2 ">
         <input
-          className="px-2 shadow-md rounded text-gray-600  py-1 w-[16] md:w-[28rem] lg:w-[33rem] mt-8 font-thin focus:outline-none "
+          className="px-2 shadow-md  rounded-tl-md  rounded-bl-md  pl-5 text-gray-600  py-2 w-[16] md:w-[28rem] lg:w-[33rem] mt-8 font-thin focus:outline-none "
           placeholder="Search"
           type="search"
           value={name}
@@ -21,7 +21,7 @@ const SearchInput = ({ name, handleSearch, setName }: Props): JSX.Element => {
         <button
           type="submit"
           onClick={handleSearch}
-          className="bg-blue-600 p-1  text-white rounded"
+          className="bg-white p-2  text-gray-500 rounded-tr-md  rounded-br-md pr-5 "
         >
           <i className="fas fa-search"></i>
         </button>
