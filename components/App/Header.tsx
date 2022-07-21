12e79896998b5da3487538eacc668afcaf014b68
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-21 12:29:08 +0100

    chore: several improvments

--- components/App/Header.tsx
+++ components/App/Header.tsx
@@ -14,24 +14,36 @@ const Header = ({ children, headerBig }: Props): JSX.Element => {
   return (
     <>
       <div
-        className="p-0 text-center relative overflow-hidden bg-no-repeat  bg-center bg-cover mb-5 md:mb-5 lg:mb-0 "
+        className={
+          headerBig
+            ? "p-0 text-center relative overflow-hidden bg-no-repeat  bg-center bg-cover mb-5 md:mb-5 lg:mb-0  h-[500px]"
+            : "p-0 text-center relative overflow-hidden bg-no-repeat  bg-center bg-cover mb-5 md:mb-5 lg:mb-0 border-b md:h-[85px] lg:h-[100px] h-[72px]"
+        }
         style={{
-          backgroundImage:
-            "url('https://res.cloudinary.com/dtycp96d8/image/upload/v1658331164/pexels-luciann-photography-3566191_1_qhokvw.jpg')",
-          height: `${headerBig ? "400px" : "250px"}`,
+          backgroundImage: `${
+            headerBig
+              ? "url('https://res.cloudinary.com/dtycp96d8/image/upload/v1658331164/pexels-luciann-photography-3566191_1_qhokvw.jpg')"
+              : ""
+          }`,
+
+          /*        height: `${headerBig ? "500px" : "100px"}`, */
         }}
       >
-        <NavBar />
+        <NavBar headerBig={headerBig} />
         <div
           className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+          style={{
+            backgroundColor: `${headerBig ? "rgba(0, 0, 0, 0.6)" : ""}`,
+          }}
         >
           <div className="flex justify-center items-center h-full">
             <div className="text-white px-5 lg:px-0 md:px-0 text-left">
-              <h4 className="font-semibold text-base w-[18rem] sm:text-xl sm:w-[24rem] md:text-2xl md:w-[30rem] lg:text-2xl w-70 lg:w-[30rem] ">
-                {" "}
-                {phrase}{" "}
-              </h4>
+              {headerBig ? (
+                <h4 className="font-semibold text-base w-[18rem] sm:text-xl sm:w-[24rem] md:text-2xl md:w-[30rem] lg:text-2xl w-70 lg:w-[30rem] ">
+                  {" "}
+                  {phrase}{" "}
+                </h4>
+              ) : null}
               {children}
             </div>
           </div>
--- components/App/NavBar.tsx
+++ components/App/NavBar.tsx
@@ -1,26 +1,50 @@
 import Link from "next/link";
 
-const NavBar = (): JSX.Element => {
+interface Props {
+  headerBig?: boolean;
+}
+
+const NavBar = ({ headerBig }: Props): JSX.Element => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg  py-2  z-20 w-full relative flex  justify-between px-5 md:px-15 lg:px-20 ">
+      <nav
+        className={
+          "navbar navbar-expand-lg  py-4  z-20 w-full relative flex  justify-between px-5 md:px-15 lg:px-20 "
+        }
+      >
         <div className="flex flex-col align-middle items-baseline ">
           <Link href={"/"}>
-            <h2 className="font-semibold text-slate-300 lg:text-4xl cursor-pointer text-base md:text-2xl">
+            <h2
+              className={
+                headerBig
+                  ? "text-slate-300 font-semibold  lg:text-4xl cursor-pointer text-base md:text-2xl"
+                  : "text-slate-600 font-semibold  lg:text-4xl cursor-pointer text-base md:text-2xl"
+              }
+            >
               Dublin
             </h2>
           </Link>
           <Link href={"/"}>
-            <h4 className="font-semibold text-white text-sm md:text-base lg:text-xl mb-6 cursor-pointer">
+            <h4
+              className={
+                headerBig
+                  ? "font-semibold text-white text-sm md:text-base lg:text-xl mb-6 cursor-pointer"
+                  : "font-semibold text-slate-400 text-sm md:text-base lg:text-xl mb-6 cursor-pointer "
+              }
+            >
               Hospitality Jobs
             </h4>
           </Link>
         </div>
-        <div className=" lg:mt-1     h-auto ">
+        <div className=" lg:mt-1 h-auto ">
           <Link href="/post">
             <button
               type="button"
-              className="inline-block px-6 py-2.5  bg-white  text-slate-600 font-bold  text-xs leading-tight  rounded shadow-md  hover:shadow-lg focus:bg-slate-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-200 active:shadow-lg transition duration-150 ease-in-out"
+              className={
+                headerBig
+                  ? "inline-block px-6 md:py-4 lg:py-6 py-3   bg-white  text-slate-600 font-bold  text-xs leading-tight  rounded shadow-md  hover:shadow-lg hover:bg-slate-100 focus:bg-slate-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-300 active:shadow-lg transition duration-150 ease-in-out"
+                  : "inline-block px-6 md:py-4 lg:py-6 py-3   bg-blue-600  text-white font-bold  text-xs leading-tight  rounded shadow-md  hover:shadow-lg focus:bg-blue-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-500 active:shadow-lg transition duration-150 ease-in-out"
+              }
             >
               Post Job
             </button>
