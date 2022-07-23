0a4ac8545ecda2eccb6a48b2f08a8505f69b3af2
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-23 15:34:41 +0100

    chore: testing using getstatic props instead of initial props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 import Layout from "../components/App/Layout";
 import { fetchAPI } from "../utils/utils";
+import { GetStaticProps } from "next/types";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,12 +17,23 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 export default MyApp;
 
-MyApp.getInitialProps = async () => {
+/* MyApp.getInitialProps = async () => {
   const jobs = await fetchAPI("/jobs", { populate: "*" });
 
   // Pass the data to our page via props
   return {
     pageProps: {
+      jobs: jobs.data,
+    },
+    revalidate: 1,
+  };
+}; */
+
+export const getStaticProps: GetStaticProps = async () => {
+  const jobs = await fetchAPI("/jobs", { populate: "*" });
+
+  return {
+    props: {
       jobs: jobs.data,
     },
     revalidate: 1,
