c517e1790c9cf67d5c526885dfd37b61421e8306
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-26 22:36:46 +0100

    chore: removed unused code added default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
-import Layout from "../components/App/Layout";
-import { fetchAPI } from "../utils/utils";
-import { GetStaticProps } from "next/types";
+import { DefaultSeo } from "next-seo";
+import Footer from "../components/App/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          description: "Fresh Dublin Hospitality jobs offers",
+          site_name: "Dublin hospitality jobs",
+        }}
+      />
+      <div className="flex flex-col min-h-screen">
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
