640a171d20485ef4dae7a4ed63a4e27ec77fcf50
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-28 18:47:42 +0100

    fix: button position because of the font weight

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -69,7 +69,7 @@ const SearchInput = ({
           className={
             router.pathname.includes("/jobs") ||
             router.pathname.includes("/post")
-              ? "bg-zinc-100 p-2  align-baseline  text-gray-500  sm:inline-block  border-slate-300 shadow-sm rounded-tr-md  rounded-br-md pr-5 "
+              ? "bg-zinc-100 p-2  align-baseline font-normal text-gray-500  sm:inline-block  border-slate-300 shadow-sm rounded-tr-md  rounded-br-md pr-5 "
               : "bg-white p-2  align-baseline text-gray-500 rounded-tr-md  rounded-br-md  pr-5 "
           }
         >
