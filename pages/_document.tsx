59347c8ddb11e9dedee39a2631f7fbed0085855a
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-30 20:50:19 +0100

    chore: adding g analityics scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
@@ -19,6 +19,24 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
             async
           ></script>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          />
+          <script
+            async
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                            page_path: window.location.pathname,
+                        });
+                        `,
+            }}
+          />
         </Head>
         <body>
           <Main />
